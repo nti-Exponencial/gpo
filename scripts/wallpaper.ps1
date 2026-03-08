280b814040a41a71eb6f8a480628bc76829800e0
Cataratas <cataratasdolly@outlook.com>
2026-03-08 16:58:27 -0300

    add hand

--- scripts/wallpaper.ps1
+++ scripts/wallpaper.ps1
@@ -2,8 +2,8 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $Wallpapers = @(
-"https://cataratas.github.io/Exponencial-GPO/assets/wallpaper_orange.jpeg",
-"https://cataratas.github.io/Exponencial-GPO/assets/wallpaper_blue.jpeg"
+    "https://cataratas.github.io/Exponencial-GPO/assets/wallpaper_orange.jpeg",
+    "https://cataratas.github.io/Exponencial-GPO/assets/wallpaper_blue.jpeg"
 )
 
 $form = New-Object Windows.Forms.Form
@@ -17,7 +17,6 @@ $panel.AutoScroll = $true
 
 $form.Controls.Add($panel)
 
-# Windows API for wallpaper change
 Add-Type @"
 using System.Runtime.InteropServices;
 public class Wallpaper {
@@ -28,36 +27,37 @@ public static extern bool SystemParametersInfo(int uAction, int uParam, string l
 
 foreach ($url in $Wallpapers) {
 
-$file = Join-Path $env:TEMP (Split-Path $url -Leaf)
+    $file = Join-Path $env:TEMP (Split-Path $url -Leaf)
 
-# Download preview image
-Invoke-WebRequest $url -OutFile $file
+    Invoke-WebRequest $url -OutFile $file
 
-$pic = New-Object Windows.Forms.PictureBox
-$pic.Image = [Drawing.Image]::FromFile($file)
-$pic.SizeMode = "Zoom"
-$pic.Width = 180
-$pic.Height = 110
-$pic.Margin = 10
+    $pic = New-Object Windows.Forms.PictureBox
+    $pic.Image = [Drawing.Image]::FromFile($file)
+    $pic.SizeMode = "Zoom"
+    $pic.Width = 180
+    $pic.Height = 110
+    $pic.Margin = 10
+    $pic.Cursor = "Hand"
 
-$pic.Tag = $url
-$pic.Add_Click({
-    $Wallpaper = $this.Tag
-    $FileName = Split-Path $Wallpaper -Leaf
-    $Destination = "C:\Windows\Web\Wallpaper\$FileName"
+    $pic.Tag = $url
+    $pic.Add_Click({
+        $Wallpaper = $this.Tag
+        $FileName = Split-Path $Wallpaper -Leaf
+        $Destination = "C:\Windows\Web\Wallpaper\$FileName"
 
-    Invoke-WebRequest $Wallpaper -OutFile $Destination
+        Invoke-WebRequest $Wallpaper -OutFile $Destination
 
-    Set-ItemProperty "HKCU:\Control Panel\Desktop" WallpaperStyle 10
-    Set-ItemProperty "HKCU:\Control Panel\Desktop" TileWallpaper 0
+        Set-ItemProperty "HKCU:\Control Panel\Desktop" WallpaperStyle 10
+        Set-ItemProperty "HKCU:\Control Panel\Desktop" TileWallpaper 0
 
-    [Wallpaper]::SystemParametersInfo(20,0,$Destination,3)
-})
+        [Wallpaper]::SystemParametersInfo(20,0,$Destination,3)
+    })
 
-$panel.Controls.Add($pic)
+    $panel.Controls.Add($pic)
 }
 
 $form.Add_FormClosing({
     [System.Windows.Forms.Application]::Exit()
 })
+
 $form.ShowDialog()
