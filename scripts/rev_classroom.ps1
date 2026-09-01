6e7ba6c825d5e08da559d2d5eb342eca5a6c29d5
nti-exponencial <nti@colegioexponencial.com.br>
2026-09-01 16:11:18 -0300

    rev

--- scripts/rev_classroom.ps1
+++ scripts/rev_classroom.ps1
@@ -7,33 +7,20 @@ if ($null -eq $profilePath) {
     return
 }
 reg load HKU\TempHive "$profilePath\NTUSER.DAT"
-$username = Split-Path $profilePath -Leaf
-Set-LocalUser -Name $username -PasswordNeverExpires $true
-net user $username /PasswordChg:No
 
 New-Item -Path "HKU:\TempHive\Software\Policies\Google\Chrome\" -Force | Out-Null
 
-New-ItemProperty -Path "HKU:\TempHive\Software\Policies\Google\Chrome" `
-    -Name "BrowserThemeColor" -PropertyType String -Value "#1F1F1F" -Force
+Remove-ItemProperty -Path $chromePath -Name "BrowserThemeColor" `
+    -ErrorAction SilentlyContinue
 
-New-ItemProperty -Path "HKU:\TempHive\Software\Policies\Google\Chrome" `
-    -Name "NTPCustomBackgroundEnabled" -PropertyType DWord -Value 0 -Force
+Remove-ItemProperty -Path $chromePath -Name "NTPCustomBackgroundEnabled" `
+    -ErrorAction SilentlyContinue
 
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name "NoChangingLockScreen" -PropertyType DWord -Value 1 -Force
+# Remove the ActiveDesktop wallpaper restriction
+$desktopPath = "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop"
+Remove-ItemProperty -Path $desktopPath -Name "NoChangingWallPaper" `
+    -ErrorAction SilentlyContinue
 
-New-Item -Path "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" -Force | Out-Null
-New-ItemProperty -Path "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" -Name "NoChangingWallPaper" -PropertyType DWord -Value 1 -Force
-
-powercfg /change monitor-timeout-ac 0
-powercfg /change monitor-timeout-dc 0
-
-powercfg /change standby-timeout-ac 0
-powercfg /change standby-timeout-dc 0
-
-powercfg /hibernate on
-powercfg /change hibernate-timeout-ac 60
-powercfg /change hibernate-timeout-dc 60
 
 [gc]::Collect()
 [gc]::WaitForPendingFinalizers()
