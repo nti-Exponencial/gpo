bca6c87063425bd790901fcb7086da434b49d460
nti-exponencial <nti@colegioexponencial.com.br>
2026-09-10 14:35:11 -0300

    read

--- scripts/lab.ps1
+++ scripts/lab.ps1
@@ -91,6 +91,16 @@ foreach ($path in $targets) {
     }
 
     Get-ChildItem $path -ErrorAction SilentlyContinue | ForEach-Object {
+        Write-Host "`nKEY: $($_.PSChildName)" -ForegroundColor Yellow
+
+        $props = Get-ItemProperty $_.PSPath -ErrorAction SilentlyContinue
+
+        $props.PSObject.Properties |
+            Where-Object { $_.Name -notmatch '^PS' } |
+            ForEach-Object {
+                Write-Host "  $($_.Name) = $($_.Value)"
+            }
+        
         $keyName = $_.PSChildName
         $keyPath = $_.PSPath
         $properties = Get-ItemProperty $keyPath -ErrorAction SilentlyContinue
@@ -108,7 +118,6 @@ foreach ($path in $targets) {
     }
 }
 
-Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
 [gc]::Collect()
 [gc]::WaitForPendingFinalizers()
 reg unload HKU\TempHive
